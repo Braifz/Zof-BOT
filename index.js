1690f67f81fdf1d5f803e5fd0cf9d249689983c3
braifz <braian94lp@gmail.com>
2020-08-31 00:02:48 -0300

    Bot Zof

---
+++ index.js
@@ -0,0 +1,25 @@
+const Discord = require("discord.js");
+const config = require("./config.json");
+
+const client = new Discord.Client();
+
+const prefix = "$";
+
+client.on("message", function(message) {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
+
+  const commandBody = message.content.slice(prefix.length);
+  const args = commandBody.split(' ');
+  const command = args.shift().toLowerCase();
+
+  if (command === "hola") {
+    message.reply('Hola!...Solo soy un bot que saluda hasta el momento! Proximamente haré mas cosas :)');
+  } else if (command === 'chau') {
+    message.reply('Hasta luego!');
+  }
+
+  
+});
+
+client.login(config.BOT_TOKEN);

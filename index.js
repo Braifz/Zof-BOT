ef33167a6651b6bd86e3cf2fdf16be181825bf00
braifz <braian94lp@gmail.com>
2020-09-17 10:23:35 -0300

    :)

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const client = new Discord.Client();
 
 const prefix = "%";
 
-    
+
 client.on("message", function(message) {
   if (message.author.bot) return;
   if (!message.content.startsWith(prefix)) return;
@@ -15,25 +15,12 @@ client.on("message", function(message) {
   const command = args.shift().toLowerCase();
 
   if (command === "hola") {
-<<<<<<< HEAD
-    message.reply('Hola!...Solo soy un bot que saluda hasta el momento! Próximamente haré más cosas :)');
-  } 
-    else if (command === 'chau') {
-    message.reply('Hasta luego! Bueno... también me despido!');
-  } 
-    else if (command === 'to do') {
-       
-    }
-
-  
-=======
     message.reply('Hola!...Solo soy un bot que saluda hasta el momento! Proximamente haré mas cosas :)');
   } else if (command === 'chau') {
     message.reply('Hasta luego!');
   } else if (command === `server`) {
     message.reply(`This server's name is: ${message.guild.name}`);
   }
->>>>>>> 215e83bf897f7ac47c71d6405008f01fadc9a242
 
   
 });
